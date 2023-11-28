79708501ad997368e05dd723d1aab2b975412ef3
AndressaSRodrigues <srandressa2@gmail.com>
2023-11-28 13:11:35 -0300

    Set up - vite w/ react-typescript

---
+++ notes-react/src/components/LoginForm.tsx
@@ -0,0 +1,11 @@
+export default function LoginForm(){
+    return (
+        <>
+        <form>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Log In</button>
+        </form>
+        </>
+    )
+}
